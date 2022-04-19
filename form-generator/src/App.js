507b1cdd831867df17d1975126520da97d44cad0
reng <reng412@gmail.com>
2022-04-19 14:57:38 -0700

    Add header and sample data to App.js

--- form-generator/src/App.js
+++ form-generator/src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const sampleData = [
+    {
+      tag: "input",
+      name: "first_name",
+      type: "text",
+      human_label: "First Name",
+    },
+    {
+      tag: "input",
+      name: "last_name",
+      type: "text",
+      human_label: "Last Name",
+    },
+    {
+      tag: "input",
+      name: "email",
+      type: "email",
+      human_label: "Email Address",
+    },
+    {
+      tag: "input",
+      name: "phone_number",
+      type: "text",
+      human_label: "Phone Number",
+    },
+    {
+      tag: "input",
+      name: "job_title",
+      type: "text",
+      human_label: "Job Title",
+    },
+    {
+      tag: "input",
+      name: "date_of_birth",
+      type: "date",
+      human_label: "Date of Birth",
+    },
+    {
+      tag: "input",
+      name: "parental_consent",
+      type: "checkbox",
+      human_label: "Parental Consent",
+      conditional: {
+        name: "date_of_birth",
+        show_if: (value) => {
+          const now = new Date();
+          return (
+            new Date(value) >=
+            new Date(now.getFullYear() - 13, now.getMonth(), now.getDate())
+          );
+        },
+      },
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Form Generator</h1>
+        <p>A generator that dynamically creates forms from JSON</p>
       </header>
     </div>
   );
